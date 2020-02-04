17ce91e25675493cf42a4fcfeb821fc4991db8ef
veertikothari <veertikothari@gmail.com>
2020-02-04 16:40:50 +0530

    sketch

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
@@ -35,8 +36,12 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+constlog= new Log (200,200,100,PI/2);
+Slingshot=new slingshot(bird.body,constlog.body);
+
 
 }
+
 
 function draw(){
     background(backgroundImg);
@@ -60,4 +65,6 @@ function draw(){
 
     bird.display();
     platform.display();
+    constlog.display();
+    Slingshot.display();
 }
